1bbadb3c73ac958833cf0e8a29005b72f29d13bb
Khushwant Singh Chouhan <115687208+alphakhush2201@users.noreply.github.com>
2025-03-28 02:56:33 +0530

    Update Login.jsx
    minor updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,7 +41,7 @@ export default function Login() {
             Sign in to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
-            Use eve.holt@reqres.in with any password
+            Use eve.holt@reqres.in 
           </p>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
